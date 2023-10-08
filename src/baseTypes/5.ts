15796842ddda5557515c163654f5ecb741731e1d
Ihor Leonov <ihorleonow@gmail.com>
2023-10-08 13:07:14 +0300

    UPD | resolve all tasks

--- src/baseTypes/5.ts
+++ src/baseTypes/5.ts
@@ -3,5 +3,12 @@
   І так само визначте змінну, яка може приймати тільки одне з двох рядкових значень: 'enable' або 'disable' (literal type)?
 */
 
-let union;
-let literal;
+enum Toggle {
+  "enable",
+  "disable",
+}
+
+let union: string | number;
+let literal: Toggle;
+
+literal = Toggle.disable;
